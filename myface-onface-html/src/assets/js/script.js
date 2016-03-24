16b8bad83cb0e39e0b1c83f02c1cfcf3f531fce2
unknown <lee.le@VNSGNICT023.itl.com>
2016-03-24 15:58:41 +0700

    checkout process full

--- myface-onface-html/src/assets/js/script.js
+++ myface-onface-html/src/assets/js/script.js
@@ -7,4 +7,18 @@ $(document).ready(function() {
 	$('.select-amount').selectpicker({
 	  style: 'amount-dropdown'
 	});
+
+	//open login-form
+	$('.btn-login').click(function(){
+		$('.row-login-2, .row-login-3').show();
+		$(this).parent().hide();
+		$('.box-log').hide('slow');
+    $('.login-form').show('slow');
+  });
+  $('.btn-register').click(function(){
+  	$('.row-login-2, .row-login-3').show();
+		$(this).parent().hide();
+		$('.box-log').hide('slow');
+    $('.register-form').show('slow');
+  });
 });
